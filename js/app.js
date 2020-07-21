d6be9b30b7e6a4190a7e4f260846a08d55059428
Julian <julianctni@mailbox.org>
2020-07-21 12:36:19 +0200

    Replace fa font with svgs

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ import Vue from 'vue';
 import education from './../data/education.json';
 import projects from './../data/projects.json';
 import workExperience from './../data/work_experience.json';
-import '@fortawesome/fontawesome-free/css/brands.css';
-import '@fortawesome/fontawesome-free/css/solid.css';
-import '@fortawesome/fontawesome-free/css/fontawesome.css';
+//import '@fortawesome/fontawesome-free/css/brands.css';
+//import '@fortawesome/fontawesome-free/css/solid.css';
+//import '@fortawesome/fontawesome-free/css/fontawesome.css';
+//import '@fortawesome/fontawesome-free/svgs/brands'
 import './../css/style.css';
 
 const removeNoPreloadTransitionClasses = () => {
