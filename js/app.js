30b8fc825b328b88bd91a8084f73b954a1a5d953
Julian <julianctni@mailbox.org>
2020-07-21 11:23:56 +0200

    Update fa imports, update dev script

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import education from './../data/education.json';
 import projects from './../data/projects.json';
 import workExperience from './../data/work_experience.json';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import './../css/style.css';
 
 const removeNoPreloadTransitionClasses = () => {
