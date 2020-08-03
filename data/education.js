4259de82e51ea7413575334537936e8dcee10ddf
Julian <julianctni@mailbox.org>
2020-08-03 20:43:48 +0200

    Refactor js, converted json files to js modules

---
+++ data/education.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    period: "04/2016 – 09/2018",
+    title: "M.Sc. Media Computer Science",
+    location: "Technische Universität Dresden, Germany",
+  },
+  {
+    period: "10/2012 – 04/2016",
+    title: "B.Sc. Media Computer Science",
+    location: "Technische Universität Dresden, Germany",
+  },
+  {
+    period: "09/2001 – 07/2010",
+    title: "Allgemeine Hochschulreife (Abitur)",
+    location: "Jack-Steinberger-Gymnasium, Bad Kissingen, Germany",
+  },
+];
---
+++ data/projects.js
@@ -0,0 +1,71 @@
+export default [
+  {
+    title: "mensaDD",
+    description:
+      "mensaDD is an Android app for retrieving the menus of student canteens in and around Dresden. The main reason for this project was my personal dissatisfaction with available canteen apps. Besides displaying the menu plans, mensaDD supports the visualisation of all canteens on a map as well as checking the current credit balance of the canteen card. Vegeterian meals can optionally be highlighted. In addition to the app itself, the project includes a server-side crawler that regularly retrieves the offers from the Studentenwerk's website.",
+    url: "https://github.com/julianctni/mensaDD",
+    tags: ["Android", "App", "Java", "Mapbox", "Python"],
+  },
+  {
+    title: "OUTPUT.DD",
+    description:
+      "OUTPUT.DD is an annual one-day fair of the faculty of computer science at the TU Dresden. The app includes the program, a map view of the event area, an integrated Twitter feed and a gamification component. As part of my master thesis I redesigned the complete UI as well as parts of the game concept. I have continued this work during my work as a research assistant.",
+    url:
+      "https://play.google.com/store/apps/details?id=de.tud.android.outputdd",
+    tags: [
+      "Android",
+      "App",
+      "Java",
+      "Realm",
+      "Gamification",
+      "Research project",
+      "Study project",
+    ],
+  },
+  {
+    title: "Dresden Vegan",
+    description:
+      "Dresden Vegan is an Android app where users can quickly find out where they can eat or shop vegan in Dresden. The app includes restaurants, cafés, bakeries, grocery stores and events. These can be displayed in lists or on a map. Dresden Vegan was the first Android app I developed. This was done in cooperation with a friend from university. Unfortunately the app is currently not being developed further due to time constraints.",
+    url: "https://github.com/julianctni/ddvegan",
+    tags: ["Android", "App", "Java"],
+  },
+  {
+    title: "fastvpn",
+    description:
+      'As part of the research project "fastvpn - Network Infrastructure for Industry 4.0", I developed a web application for the administration of networks together with two fellow students during my master\'s degree. Besides the client application with dashboard, editable network graph and logging system we also developed the server component. The communication took place via HTTP requests and websockets.',
+    url: "https://imld.de/research/research-projects/fastvpn/",
+    tags: [
+      "Web",
+      "Dashboard",
+      "Graphs",
+      "JavaScript",
+      "Materialize CSS",
+      "Cytoscape.js",
+      "node.js",
+      "Websockets",
+      "Study project",
+    ],
+  },
+  {
+    title: "Verbogene Relationen",
+    description:
+      "\"Verborgene Relationen\" is a museum application that I developed together with a fellow student during my master's degree. The application was developed for the special exhibition of SKD Dresden on Veronese's Cuccina cycle and consists of two parts. An interactive graph, which breaks down the information shown in the painting, is displayed on a multi-touch tabletop display. In the second part of the application, the corresponding parts of the painting are highlighted on a large screen or beamer image.",
+    tags: [
+      "Museum",
+      "Tabletop",
+      "Touch",
+      "D3.js",
+      "JavaScript",
+      "node.js",
+      "Websockets",
+      "Study project",
+    ],
+  },
+  {
+    title: "Website 50th anniversary of computer science education",
+    description:
+      "For the 50th anniversary of computer science education at the TU Dresden (and other universities) I developed a website during my employment as a research assistant. It contains all information about the program as well as a map view which shows the different locations where events take place. During development, special emphasis was placed on responsiveness.",
+    url: "https://festtage.inf.tu-dresden.de",
+    tags: ["Website", "React.js", "JavaScript", "Mapbox", "Reactstrap"],
+  },
+];
