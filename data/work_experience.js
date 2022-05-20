f2dccf1bc9f3a1e65176dc7647e6737275047ab9
Julian Catoni <j.catoni@elco-automation.de>
2022-05-20 13:30:17 +0200

    Fix language in titles

--- data/work_experience.js
+++ data/work_experience.js
@@ -1,27 +1,27 @@
 export default [
   {
     period: "10/2020 – today",
-    title: "Software developer frontend/apps",
+    title: "Software Developer Frontend/Apps",
     task:
       "Developing ELCO's AppDesigner",
     location: "ELCO Industrial Automation, Dresden, Germany",
   },
   {
     period: "10/2018 – 09/2020",
-    title: "Research assistant",
+    title: "Research Assistant",
     task:
       "Heavily involved in teaching at chair of software engineering, co-organizer of OUTPUT.DD app",
     location: "Technische Universität Dresden, Germany",
   },
   {
     period: "10/2013 – 09/2017",
-    title: "Student assistant",
+    title: "Student Assistant",
     task: "Supervising the law faculty's computer pool",
     location: "Technische Universität Dresden, Germany",
   },
   {
     period: "08/2010 – 01/2011",
-    title: "Community service",
+    title: "Community Service",
     task: "Care of patients with restricted mobility in everyday therapy",
     location: "Luitpold Klinik Heiligenfeld, Bad Kissingen, Germany",
   },
